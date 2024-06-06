e298445a8833f1af8857f33840c00068f12feb10
Ashwin Adhikari <adhikariaswin7@gmail.com>
2024-06-06 22:07:29 +0545

    Day6: CRUD

--- apps/urls.py
+++ apps/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     
 path('admin/',admin.site.urls),
-path('drinks/',views.drink_list)
-
+path('drinks/',views.drink_list),
+path('drinks/<int:id>', views.drink_detail)
 ]
