53f91ef479f4751b4f4ecd76d4101ac3cf55d5ea
Ashwin Adhikari <adhikariaswin7@gmail.com>
2024-06-19 23:10:00 +0545

    login auth

--- ordermanagement/views.py
+++ ordermanagement/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 from rest_framework import viewsets, status
 from .serializers import *
 from rest_framework.response import Response
-from rest_framework.decorators import action, api_view
+from rest_framework.decorators import action, api_view, authentication_classes,permission_classes
+from rest_framework.authentication import SessionAuthentication,TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 
@@ -71,7 +73,12 @@ class AuthViewSet(viewsets.ModelViewSet):
 
     @api_view(["POST"])
     def login(request):
-        return Response()
+        user = get_object_or_404(User, username=request.data["username"])
+        if not user.check_password(request.data["password"]):
+            return Response({"detail": "Not Found"}, status=status.HTTP_404_NOT_FOUND)
+        token, created = Token.objects.get_or_create(user=user)
+        serializer = UserSerializer(instance=user)
+        return Response({"token": token.key, "user": serializer.data})
 
     @api_view(["POST"])
     def signup(request):
@@ -89,5 +96,8 @@ class AuthViewSet(viewsets.ModelViewSet):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @api_view(["GET"])
+    @authentication_classes([SessionAuthentication,TokenAuthentication])
+    @permission_classes([IsAuthenticated])
     def test_token(request):
-        return Response()
+
+        return Response("passed for {}".format(request.user.email))
